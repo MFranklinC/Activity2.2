9830d11dea5a1133958fccc57c6a597e8c02952e
MFranklinC <23-15964@g.batstate-u.edu.ph>
2024-11-08 15:00:53 +0800

    Update InheritanceSample.java

--- InheritanceSample.java
+++ InheritanceSample.java
@@ -1,9 +1,7 @@
 public class InheritanceSample {
     public static void main(String[] args) {
-        // Creating an instance of Student
         Student student = new Student("Mar Franklin Caraig", "Barangay Sambat", "BSIT", 2);
         
-        // Printing Student details
         System.out.println("Student Details:");
         System.out.println("Name: " + student.getName());
         System.out.println("Address: " + student.getAddress());
@@ -19,92 +17,5 @@ public class InheritanceSample {
         System.out.println("Address: " + staff.getAddress());
         System.out.println("School: " + staff.getSchool());
         System.out.println("Salary: " + staff.getSalary());
-    }
-}
-class Person {
-
-    protected String Name;
-    protected String Address;
-
-
-    public void setName(String name) {
-        this.Name = name;
-    }
-
-
-    public String getName() {
-        return Name;
-    }
-
-
-    public void setAddress(String address) {
-        this.Address = address;
-    }
-
-
-    public String getAddress() {
-        return Address;
-    }
-}
-class Staff extends Person {
-    
-    private String School;
-    private double Salary;
-
-    
-    public Staff(String Name, String Address, String School, double Salary) {
-        this.Name = Name;
-        this.Address = Address;
-        this.School = School;
-        this.Salary = Salary;
-    }
-
-    
-    public void setSchool(String school) {
-        this.School = school;
-    }
-
-    public String getSchool() {
-        return School;
-    }
-
-    
-    public void setSalary(double salary) {
-        this.Salary = salary;
-    }
-
-    public double getSalary() {
-        return Salary;
-    }
-}
-class Student extends Person {
-
-    private String Program;
-    private int Year;
-
-
-    public Student(String Name, String Address, String Program, int Year) {
-        this.Name = Name;
-        this.Address = Address;
-        this.Program = Program;
-        this.Year = Year;
-    }
-
-
-    public void setProgram(String program) {
-        this.Program = program;
-    }
-
-    public String getProgram() {
-        return Program;
-    }
-
-
-    public void setYear(int year) {
-        this.Year = year;
-    }
-
-    public int getYear() {
-        return Year;
     }
 }
